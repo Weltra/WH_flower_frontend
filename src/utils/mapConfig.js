7c7b10c31cb71fc9b49fbb3716daf2163edfd23a
welt <83679711+weltme@users.noreply.github.com>
2023-05-30 22:52:29 +0800

    Update mapConfig.js

--- src/utils/mapConfig.js
+++ src/utils/mapConfig.js
@@ -1,4 +1,4 @@
 export default {
-    JSAPIKey: '09e6391bbef7070a5fc4c6db5e306338',
-    WebAPIKey: '647b17e5508e7ae00b122df635fa4e25'
+    JSAPIKey: '你的高德地图JSAPIKey',
+    WebAPIKey: '你的高德地图WebAPIKey'
 }
