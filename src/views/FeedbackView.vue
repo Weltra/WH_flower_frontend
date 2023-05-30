4722118ce450eb26069172984a17511836d4bd74
weltme <tjlwelt@foxmail.com>
2023-05-30 21:35:51 +0800

    添加图片上传组件

--- src/views/FeedbackView.vue
+++ src/views/FeedbackView.vue
@@ -1,47 +1,54 @@
 <template>
     <div>
-        <Header :routelist="routelist"></Header>
+        <header>
+            <Header :routelist="routelist"></Header>
+        </header>
         <div class="body">
             <div id="contact-form" class="contact-form">
-                <h1 class="contact-form_title">Contact Form</h1>
+                <h1 class="contact-form_title"><i class="el-icon-edit"></i>向我们推荐赏花点！</h1>
                 <div class="separator"></div>
-                <div v-if="isSending" class="loading">Sendig...</div>
-                <form class="form" @submit="onSubmit">
-                    <input style="background-color: white;" required name="name" v-model='contact.name' placeholder="Name"
-                        type="text" autocomplete="off">
-                    <input style="background-color: white;" required name="email" v-model="contact.email"
-                        placeholder="E-mail" type="email" autocomplete="off">
-                    <textarea style="background-color: white; height: 200px;" name="message" v-model="contact.message"
-                        rows="4" placeholder="Message"></textarea>
+                <div class="form">
+                    <input style="background-color: white;" v-model='point.name' placeholder="赏花点名称" type="text"
+                        autocomplete="off">
+                    <input style="background-color: white;" v-model='point.description' placeholder="地址" type="text"
+                        autocomplete="off">
+                    <div>
+                        <input style="background-color: white;" v-model="point.longitude" placeholder="经度" type="number"
+                            autocomplete="off">
+                        <input style="background-color: white;" v-model="point.latitude" placeholder="纬度" type="number"
+                            autocomplete="off">
+                    </div>
+                    <input style="background-color: white;" v-model="point.ticket" placeholder="门票价格" type="text"
+                        autocomplete="off">
+                    <input style="background-color: white;" v-model="point.open_time" placeholder="开放时间" type="text"
+                        autocomplete="off">
+                    <input style="background-color: white;" v-model="point.traffic" placeholder="交通方式" type="text"
+                        autocomplete="off">
+                    <input style="background-color: white;" v-model="point.flower_class" placeholder="花卉种类" type="text"
+                        autocomplete="off">
                     <p style="color: #5d655f; font-weight: 550; font-size: 17px;"><i class="el-icon-picture"></i> 添加赏花点图片：
                     </p>
                     <div class="upload">
                         <div class="upload-file">
-                            <el-upload class="Upload" ref="fileUpload" list-type="picture-card" action=""
-                                :http-request="fnUploadRequest" :on-success="handleSuccess" :on-error="handleError"
-                                :before-upload="handleUpload" on-remove="" multiple>
-                                <i class="el-icon-plus"></i>
-                                <template #tip>
-                                    <div style="font-size: 13px;color: #919191; margin-top: 5px;">
-                                        单次限制上传一张图片
-                                    </div>
-                                </template>
-                            </el-upload>
+                            <ImageUploader :avatar="avatar" :region="options_OSS.region"
+                                :accessKeyId="options_OSS.accessKeyId" :accessKeySecret="options_OSS.accessKeySecret"
+                                :bucket="options_OSS.bucket" @avatarUrl="getAvatarUrl" @deleteAvatar="deleteAvatar">
+                            </ImageUploader>
                         </div>
                     </div>
-                    <button class="button">Send</button>
-                </form>
+                    <el-button class="button" @click="submit">提交</el-button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { uploadOSS } from '@/utils/oss';
 import Header from '@/components/Header.vue';
+import ImageUploader from '@/components/ImageUpload.vue'
 
 export default {
-    components: { Header },
+    components: { Header, ImageUploader },
     data() {
         return {
             routelist: [
@@ -52,101 +59,60 @@ export default {
                 { route: '/Route', name: '路线展示' },
                 { route: '/User', name: '个人中心' }
             ],
-            contact: {
+            point: {
                 name: '',
-                email: '',
-                message: '',
+                description: '',
+                longitude: '',
+                latitude: '',
+                imgURL: '',
+                traffic: '',
+                open_time: '',
+                ticket: '',
+                flower_class: ''
             },
-            isSending: false,
-            file: [],
-            fileList: []
+            avatar: '',
+            options_OSS: {
+                region: 'oss-cn-hangzhou',
+                accessKeyId: 'LTAI5tFQrt6TNg6k2SoRtnmW',
+                accessKeySecret: 'arpTCv67LVeoOy2CP8sZqF4DCOOBk6',
+                bucket: 'picture-tjl'
+            },
         }
     },
-
     methods: {
-        clearForm() {
-            for (let field in this.contact) {
-                this.contact[field] = ''
-            }
+        getAvatarUrl(avatarUrl) {
+            console.log("图片url:", avatarUrl);
+            this.point.imgURL = avatarUrl;
         },
-
-        //Handler for submit
-        onSubmit(evt) {
-            evt.preventDefault();
-
-            this.isSending = true;
-
-            setTimeout(() => {
-                // Build for data
-                let form = new FormData();
-                for (let field in this.contact) {
-                    form.append(field, this.contact[field]);
+        deleteAvatar() {
+        },
+        submit() {
+            this.$axios.post('http://127.0.0.1:8000/add_map_point', this.point
+            ).then((res) => {
+                if (res.data.code == '0000') {
+                    this.$message({
+                        message: '地图点添加成功！',
+                        type: 'success'
+                    });
+                    this.point = {
+                        name: '',
+                        description: '',
+                        longitude: '',
+                        latitude: '',
+                        imgURL: '',
+                        traffic: '',
+                        open_time: '',
+                        ticket: '',
+                        flower_class: ''
+                    }
                 }
-
-                // Send form to server
-                this.$http.post('/app.php', form).then((response) => {
-                    console.log(response);
-                    this.clearForm();
-                    this.isSending = false;
-                }).catch((e) => {
-                    console.log(e)
-                });
-
-            }, 1000);
-        },
-
-        handleUpload(file, fileList) {
-            console.log(file, fileList);
-            var testmsg = file.name.substring(file.name.lastIndexOf('.') + 1)
-            const extension = testmsg === 'xlsx' || testmsg === 'xls' || testmsg === 'docx' || testmsg === 'doc'
-                || testmsg === 'pdf' || testmsg === 'zip' || testmsg === 'rar' || testmsg === 'ppt' || testmsg === 'txt' || testmsg === 'jpeg' || testmsg === 'jpg' || testmsg === 'png'
-
-            const isLimit100M = file.size / 1024 / 1024 < 100
-            var bool = false;
-            if (extension && isLimit100M) {
-                bool = true;
-            } else {
-                bool = false;
-            }
-            if (!extension) {
-                this.$message.error('请选择文件类型！');
-                return bool;
-            }
-            if (!isLimit100M) {
-                this.$message.error('上传失败，不能超过100M！');
-                return bool;
-            }
-            return bool;
-        },
-        handleSuccess(res) {
-            if (res) {
-                this.$emit('fileData', res)
-                this.$message.success("上传成功！");
-            }
-        },
-        handleError(err) {
-            if (err) {
-                this.$message.error('上传失败！');
-            }
-        },
-        // 上传图片
-        async fnUploadRequest(options) {
-            try {
-                // console.log(options);
-                let file = options.file; // 拿到 file
-                let res = await uploadOSS('WuHan_Flower/' + file.name, file)
-                console.log(res);
-                // 返回数据
-                this.$emit("fileData", res);
-                this.$message.success("上传成功！");
-                this.fileList.push({
-                    name: res.fileName,
-                    url: res.fileUrl
-                });
-                console.log(this.fileList)
-            } catch (e) {
-                this.$message.error('上传失败！');
-            }
+                else if (res.data.code == '0001') {
+                    this.$message.error('参数错误，地图点添加失败！');
+                }
+            }).catch(err => {
+                console.log(err)
+                this.$message.error('地图点添加失败！');
+            })
         },
     }
 }
@@ -159,7 +125,7 @@ export default {
 .body {
     background: white;
     font-family: 'Roboto', sans-serif;
-    height: 92vh;
+    height: 101vh;
 }
 
 .contact-form {
@@ -187,8 +153,16 @@ export default {
     margin-bottom: 10px;
 }
 
-.contact-form input[type="email"],
-.contact-form input[type="text"],
+.contact-form input[type="number"] {
+    width: 49%;
+}
+
+.contact-form input[type="number"]:last-of-type {
+    width: 49%;
+    margin-left: 2%;
+}
+
+.contact-form input,
 .contact-form textarea {
     border: solid 2px #A9A9A9;
     font-family: 'Roboto', sans-serif;
@@ -216,9 +190,7 @@ export default {
     border: solid 1px #ed556a;
 }
 
-.contact-form input[type="email"],
-.contact-form input[type="text"],
-.contact-form textarea,
+.contact-form input,
 .contact-form .button {
     font-size: 15px;
     border-radius: 3px
