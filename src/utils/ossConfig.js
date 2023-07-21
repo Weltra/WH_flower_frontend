0c3e149a193ca6bc0d529a6cbc247a444c99d327
Weltra <2124954906@qq.com>
2023-07-21 10:58:20 +0800

    更新oss设置

--- src/utils/ossConfig.js
+++ src/utils/ossConfig.js
@@ -1,8 +1,8 @@
 export default {
-    options_OSS: {
-        region: '你的阿里云Oss所在的区域',
-        accessKeyId: '你的阿里云Oss的key值',
-        accessKeySecret: '你的阿里云Oss的key的密码',
-        bucket: '你的阿里云Oss的bucket'
-    },
+  options_OSS: {
+    region: '你的阿里云Oss所在的区域',
+    accessKeyId: '你的阿里云Oss的key值',
+    accessKeySecret: '你的阿里云Oss的key的密码',
+    bucket: '你的阿里云Oss的bucket',
+  },
 }
