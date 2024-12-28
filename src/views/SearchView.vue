48c170290b998339e8f127ad78631b48cb90e02f
Weltra <2124954906@qq.com>
2024-12-28 14:57:38 +0800

    修改搜索结果宽度问题

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,98 +1,125 @@
 <template>
   <div>
-
-    <body>
-      <header class="page-header">
-        <div class="site-name">
-          <img src="../assets/logo.png" class="logo">
-        </div>
-        <div class="menu-container">
-          <nav>
-            <ul class="menu" role="list" style="list-style-type:none">
-              <li><router-link to="/Home"><a>主页</a></router-link></li>
-              <li><router-link to="/Map"><a>赏花地图</a></router-link></li>
-              <li><router-link to="/Feedback"><a>我的发现</a></router-link></li>
-              <li><router-link to="/Route"><a>路线展示</a></router-link></li>
-              <li><router-link to="/User"><a style="font-size: 18px;"><i class="el-icon-user-solid"></i></a></router-link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div class="search-wrapper">
-          <div class="search-bar">
-            <div class="search-controls">
-              <button class="magnifier material-icons" @click="search">
-                <span>search</span>
-              </button>
-              <input type="text" placeholder="搜索赏花点名称或花卉品种" v-model="searchText" style="background-color: white">
-              <button class="magnifier material-icons" @click="search">
-                <span>search</span>
-              </button>
-            </div>
+    <header class="page-header">
+      <div class="site-name">
+        <img src="../assets/logo.png" class="logo" />
+      </div>
+      <div class="menu-container">
+        <nav>
+          <ul class="menu" role="list" style="list-style-type: none">
+            <li>
+              <router-link to="/Home"><a>主页</a></router-link>
+            </li>
+            <li>
+              <router-link to="/Map"><a>赏花地图</a></router-link>
+            </li>
+            <li>
+              <router-link to="/Feedback"><a>我的发现</a></router-link>
+            </li>
+            <li>
+              <router-link to="/Route"><a>路线展示</a></router-link>
+            </li>
+            <li>
+              <router-link to="/User"
+                ><a style="font-size: 18px"
+                  ><i class="el-icon-user-solid"></i></a
+              ></router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="search-wrapper">
+        <div class="search-bar">
+          <div class="search-controls">
+            <button class="magnifier material-icons" @click="search">
+              <span>search</span>
+            </button>
+            <input
+              type="text"
+              placeholder="搜索赏花点名称或花卉品种"
+              v-model="searchText"
+              style="background-color: white"
+            />
+            <button class="magnifier material-icons" @click="search">
+              <span>search</span>
+            </button>
           </div>
         </div>
-      </header>
+      </div>
+    </header>
 
-      <main>
-        <ul>
-          <li v-for="item in items" :key="item.id" class="skeleton">
-            <div class="skeleton__block">
-              <img :src="item.imgURL" alt="" />
-            </div>
-            <div class="skeleton__block">
-              <h3>{{ item.name }}</h3>
-              <p><b><i class="el-icon-map-location"></i>位置：</b>{{ item.description }}</p>
-            </div>
-            <div class="skeleton__block">
-              <p><b><i class="el-icon-money"></i>门票价格：</b>{{ item.ticket }}</p>
-              <p><b><i class="el-icon-time"></i>开放时间：</b>{{ item.open_time }}</p>
-              <p><b><i class="el-icon-picture-outline-round"></i>花卉品种：</b>{{ item.flower_class }}</p>
-            </div>
-          </li>
-        </ul>
-      </main>
-    </body>
+    <main>
+      <ul class="search-result">
+        <li v-for="item in items" :key="item.id" class="skeleton">
+          <div class="skeleton__block">
+            <img :src="item.imgURL" alt="" />
+          </div>
+          <div class="skeleton__block">
+            <h3>{{ item.name }}</h3>
+            <p>
+              <b><i class="el-icon-map-location"></i>位置：</b
+              >{{ item.description }}
+            </p>
+          </div>
+          <div class="skeleton__block">
+            <p>
+              <b><i class="el-icon-money"></i>门票价格：</b>{{ item.ticket }}
+            </p>
+            <p>
+              <b><i class="el-icon-time"></i>开放时间：</b>{{ item.open_time }}
+            </p>
+            <p>
+              <b><i class="el-icon-picture-outline-round"></i>花卉品种：</b
+              >{{ item.flower_class }}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-export default ({
+export default {
   data() {
     return {
-      searchText: '',
-      items: [
-      ],
-      keywords: []
+      searchText: "",
+      items: [],
+      keywords: [],
     };
   },
   created() {
-    this.$axios.get('http://127.0.0.1:8000/map_points/', {
-    }).then((res) => {
-      this.items = res.data.Points
-    }).catch(err => {
-      console.log(err);
-      this.$message.error('加载失败，请检查网络！');
-    })
+    this.$axios
+      .get("http://127.0.0.1:8000/map_points/", {})
+      .then((res) => {
+        this.items = res.data.Points;
+      })
+      .catch((err) => {
+        console.log(err);
+        this.$message.error("加载失败，请检查网络！");
+      });
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     wordsCut(manage) {
-      let words = manage.split(",")
-      return words
+      let words = manage.split(",");
+      return words;
     },
     search() {
-      this.keywords = this.searchText.split(' ')
-      this.$axios.post('http://127.0.0.1:8000/search_points/', this.keywords).then((res) => {
-        this.items = res.data.Points
-        this.$message({
-          message: '搜索成功！',
-          type: 'success'
+      this.keywords = this.searchText.split(" ");
+      this.$axios
+        .post("http://127.0.0.1:8000/search_points/", this.keywords)
+        .then((res) => {
+          this.items = res.data.Points;
+          this.$message({
+            message: "搜索成功！",
+            type: "success",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$message.error("搜索失败，请检查网络！");
         });
-      }).catch(err => {
-        console.log(err);
-        this.$message.error('搜索失败，请检查网络！');
-      })
     },
   },
   mounted() {
@@ -105,8 +132,8 @@ export default ({
       header.classList.toggle("active", !entry.isIntersecting);
     });
     observer.observe(intercept);
-  }
-})
+  },
+};
 </script>
 
 <style scoped>
@@ -114,14 +141,14 @@ export default ({
 @import "open-props/normalize.min.css" layer(open-props);
 
 @font-face {
-  font-family: 'Material Icons';
+  font-family: "Material Icons";
   font-style: normal;
   font-weight: 400;
-  src: url("../assets/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2") format('woff2');
+  src: url("../assets/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2") format("woff2");
 }
 
 .material-icons {
-  font-family: 'Material Icons';
+  font-family: "Material Icons";
   font-weight: normal;
   font-style: normal;
   font-size: 24px;
@@ -132,20 +159,22 @@ export default ({
   white-space: nowrap;
   word-wrap: normal;
   direction: ltr;
-  -webkit-font-feature-settings: 'liga';
+  -webkit-font-feature-settings: "liga";
   -webkit-font-smoothing: antialiased;
 }
 
 body {
   font-family: system-ui, sans-serif;
-  background: linear-gradient(to bottom,
-      hsl(0, 0%, 100%) 0%,
-      hsl(219.19, 55.23%, 99.76%) 20%,
-      hsl(0, 100%, 95%) 35%,
-      hsl(0, 100%, 92%) 50%,
-      hsl(0, 100%, 89%) 65%,
-      hsl(0, 100%, 86%) 80%,
-      hsl(0, 100%, 83%) 100%);
+  background: linear-gradient(
+    to bottom,
+    hsl(0, 0%, 100%) 0%,
+    hsl(219.19, 55.23%, 99.76%) 20%,
+    hsl(0, 100%, 95%) 35%,
+    hsl(0, 100%, 92%) 50%,
+    hsl(0, 100%, 89%) 65%,
+    hsl(0, 100%, 86%) 80%,
+    hsl(0, 100%, 83%) 100%
+  );
   min-height: 100vh;
 }
 
@@ -262,15 +291,13 @@ main {
 }
 
 ul {
+  display: grid;
   list-style-type: none;
   padding: 0;
   margin: 0;
-}
-
-ul {
-  padding-top: var(--size-8);
-  display: grid;
   gap: var(--size-4);
+  padding-top: var(--size-8);
+  width: 100%;
 }
 
 .skeleton {
@@ -280,11 +307,12 @@ ul {
   padding: var(--padding);
   gap: var(--padding);
   width: 100%;
+  min-width: 100%;
+  max-width: 100%;
   display: grid;
   grid-template: 1fr 1fr / 25% 1fr;
   background: var(--surface);
   background-color: white;
-  max-inline-size: 100%;
   border-radius: 8px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
